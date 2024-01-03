989a8d8214bd96990610b2a61d6db9cd61bd6d68
Azriel Pamungkas <github.rwcoy@simplelogin.co>
2024-01-03 17:39:29 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import abiJson from "./contractJson/Chai.json";
+import { ethers } from "ethers";
+import Memos from "./components/Memos";
+import Buy from "./components/Buy";
+import Vote from "./components/Vote";
+// import chai from "./chai.png";
+// import './App.css'
+
+function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("Not connected");
+  useEffect(() => {
+    const template = async () => {
+      const contractAddres = "0x472364fC4baD07d7AEbc038159dea2A9f7BD5D32";
+      const contractABI = abiJson.abi;
+      //Metamask part
+      //1. In order do transactions on goerli testnet
+      //2. Metmask consists of infura api which actually help in connectig to the blockhain
+      try {
+        const { ethereum } = window;
+        const account = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        setAccount(account);
+        const provider = new ethers.BrowserProvider(ethereum); //read the Blockchain
+        const signer = await provider.getSigner(); //write the blockchain
+
+        const contract = new ethers.Contract(
+          contractAddres,
+          contractABI,
+          signer
+        );
+        // balance = await provider.getBalance("ethers.eth");
+        setState({ provider, signer, contract });
+
+        console.log(abiJson.abi);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    template();
+  }, []);
+
+  return (
+    <div>
+      {/* <img src={chai} className="img-fluid" alt=".." width="100%" /> */}
+      {/* <p style={{ marginTop: "10px", marginLeft: "5px" }}>
+        <small>Connected Account - {account}</small>
+      </p>
+
+      <Buy state={state} />
+      <Memos state={state} /> */}
+      <Vote />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Vote.jsx
@@ -0,0 +1,128 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import abi from "../contractJson/Voting.json";
+import Login from "./Login";
+import Finished from "./Finished";
+import Connected from "./Connected";
+// import "./App.css";
+
+function Vote() {
+  const [contract, setContract] = useState(null);
+  const contractAbi = abi.abi;
+  const contractAddress = "0x0fC2F3c5845e64A7dD6dd38bbB50F0112A25aF56";
+  const [isConnected, setIsConnected] = useState(false);
+  const [isVoteActive, setIsVoteActive] = useState(false);
+  const [isExist, setIsExist] = useState(false);
+  const [signerAddress, setSignerAddress] = useState(false);
+  const [remainingTime, setRemainingTime] = useState("");
+  const [candidates, setCandidates] = useState([]);
+  const [number, setNumber] = useState("");
+
+  useEffect(() => {
+    connectToMetamask();
+    getCandidates();
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      }
+    };
+  }, [signerAddress]);
+
+  function handleAccountsChanged(accounts) {
+    if (accounts.length > 0 && signerAddress !== accounts[0]) {
+      setSignerAddress(accounts[0]);
+    } else {
+      setIsConnected(false);
+      setSignerAddress(null);
+    }
+  }
+
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        await provider.send("eth_requestAccounts", []).then(async () => {});
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+        const contractInstance = new ethers.Contract(
+          contractAddress,
+          contractAbi,
+          signer
+        );
+        const time = await contractInstance.getRemainingTime();
+        const status = await contractInstance.getVotingStatus();
+        const voteStatus = await contractInstance.voters(signer);
+
+        setContract(contractInstance);
+        setSignerAddress(address);
+        setIsConnected(true);
+        setRemainingTime(parseInt(time, 16));
+        setIsVoteActive(status);
+        setIsExist(voteStatus);
+
+        // console.log("Metamask Connected : " + address);
+      } catch (err) {
+        // console.error(err);
+      }
+    } else {
+      console.error("Metamask is not detected in the browser");
+    }
+  }
+
+  async function vote() {
+    if (contract !== null) {
+      const tx = await contract.vote(number);
+      await tx.wait();
+      // canVote();
+    }
+  }
+
+  async function getCandidates() {
+    if (contract !== null) {
+      const candidatesList = await contract.getAllVotesOfCandiates();
+      const formattedCandidates = candidatesList.map((candidate, index) => {
+        return {
+          index: index,
+          name: candidate.name,
+          voteCount: ethers.toNumber(candidate.voteCount),
+        };
+      });
+      setCandidates(formattedCandidates);
+    }
+  }
+
+  async function handleNumberChange(e) {
+    setNumber(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      {isConnected ? (
+        isVoteActive ? (
+          <Connected
+            account={signerAddress}
+            candidates={candidates}
+            remainingTime={remainingTime}
+            number={number}
+            handleNumberChange={handleNumberChange}
+            voteFunction={vote}
+            isExist={isExist}
+          />
+        ) : (
+          <Finished />
+        )
+      ) : (
+        <Login connectWallet={connectToMetamask} />
+      )}
+    </div>
+  );
+}
+
+export default Vote;
